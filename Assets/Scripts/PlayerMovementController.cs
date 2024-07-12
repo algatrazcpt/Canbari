5a0d849ab45899eea224580fa186c80de5fef424
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-07-12 16:02:20 +0300

    basic movement

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+   public Animator animController;
+    bool moveAble = true;
+    float horizontalInput;
+    float verticalInput;
+    float walkSpeed = 20f;
+    public Rigidbody2D rg;
+    
+    public Vector2 MoveDirection()
+    {
+        return new Vector2(horizontalInput, verticalInput);
+    }
+    private void FixedUpdate()
+    {
+        if (moveAble)
+        {
+            if (Input.GetButton("Horizontal"))
+            {
+                horizontalInput = Input.GetAxis("Horizontal");
+            }
+            else
+            {
+                horizontalInput = 0f;
+            }
+
+            if (Input.GetButton("Vertical"))
+            {
+                verticalInput = Input.GetAxis("Vertical");
+            }
+            else
+            {
+                verticalInput = 0f;
+            }
+            //rg.velocity=
+            rg.velocity = MoveDirection().normalized * walkSpeed * Time.fixedDeltaTime;
+            AnimateMove();
+        }
+        else
+        {
+            rg.velocity = Vector2.zero;
+        }
+
+    }
+    private void AnimateMove()
+    {
+        animController.SetFloat("MovementX", MoveDirection().normalized.x);
+        animController.SetFloat("MovmentY", MoveDirection().normalized.y);
+    }
+}
